edbb7eb632d10425682833f300ba2536fdb41ff4
egg-user <egakuswardana@gmail.com>
2024-10-16 16:46:42 +0000

    Add dynamic file name based on time

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const {exec} = require('child_process')
-const { stdout, stderr } = require('process')
-const date = new Date()
-const day = date.toLocaleDateString().replace(/[/]/g, ".")
+const { stdout, stderr } = require('process');
+const { start } = require('repl');
+const cron = require('node-cron');
 
 //const test = (`tcpdump -w ${day}.pcap`)
-//console.log(test)
-//console.log(day)
-exec (`tcpdump arp -U -w "${day}".pcap`, (err, stdout, stderr) => {
+// console.log(date)
+const scriptCaptureArp = function() {
+    const date = new Date().getUTCDate()
+    const hours = new Date().getHours()
+    const months = new Date().getMonth()
+    const years = new Date().getFullYear()
+    const Seconds = new Date().getSeconds()
+    const NameFile = (`${hours}H-${date}-${months}-${years}`)
+    exec (`tcpdump arp -U -w ./packet/${NameFile}.pcap`, (err, stdout, stderr) => {
     if (err) {
         res.err('Command tidak dapat dijalankan')
         return
@@ -15,3 +21,12 @@ exec (`tcpdump arp -U -w "${day}".pcap`, (err, stdout, stderr) => {
     console.log(`stdout: ${stdout}`)
     console.log(`stderr: ${stderr}`)
 })
+}
+
+cron.schedule('* 0 * * * *', () => {
+    scriptCaptureArp()    
+}, {
+    timezone: "Asia/Jakarta"
+});
+
+scriptCaptureArp()
